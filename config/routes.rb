35f388752f5f257f3290e1ac86f65dd1325d5917
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-02-04 23:03:51 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'posts/create'
   get 'posts/edit'
   get 'posts/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
+  root "posts#index"
+  resources :posts
 end
